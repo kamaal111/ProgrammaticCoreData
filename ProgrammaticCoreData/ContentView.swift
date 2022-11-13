1c8a85deaea10f30bf5a9d27c659ade780618769
Kamaal <kamaal.f1@gmail.com>
2022-11-13 20:06:37 +0100

    CoreData stack is complete

--- ProgrammaticCoreData/ContentView.swift
+++ ProgrammaticCoreData/ContentView.swift
@@ -11,15 +11,65 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
+    @State private var items: [Item] = []
+
     var body: some View {
         NavigationView {
-            Text("Hello World")
+            List {
+                if items.isEmpty {
+                    Text("No items yet")
+                }
+                ForEach(items, id: \.id) { item in
+                    Text(dateFormatter.string(from: item.timestamp))
+                }
+            }
+            .navigationTitle("Programmatic CD")
+            .toolbar { ToolbarItem(placement: .navigationBarTrailing) { toolbarView } }
         }
-        .onAppear {
-            print("viewContext", viewContext)
+        .onAppear(perform: { fetchItems() })
+    }
+
+    private var toolbarView: some View {
+        Button(action: addItem) {
+            Image(systemName: "plus")
         }
     }
+
+    private func addItem() {
+        let item = Item(context: viewContext)
+        item.timestamp = Date()
+        item.id = UUID()
+
+        do {
+            try viewContext.save()
+        } catch {
+            print("error", error)
+            return
+        }
+
+        items.append(item)
+    }
+
+    private func fetchItems() {
+        let request = Item.fetchRequest()
+        let result: [Item]
+        do {
+            result = try viewContext.fetch(request)
+        } catch {
+            print("error", error)
+            return
+        }
+
+        items = result
+    }
 }
+
+private let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .medium
+    return formatter
+}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- ProgrammaticCoreData/Item.swift
+++ ProgrammaticCoreData/Item.swift
@@ -11,6 +11,13 @@ import CoreData
 public class Item: NSManagedObject, ManualManagedObject {
     static let entityName = "Item"
 
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Item> {
+        NSFetchRequest<Item>(entityName: entityName)
+    }
+
+    @NSManaged public var id: UUID
+    @NSManaged public var timestamp: Date
+
     static let entity: NSEntityDescription = {
         // Create the entity
         let entity = NSEntityDescription()
@@ -18,15 +25,22 @@ public class Item: NSManagedObject, ManualManagedObject {
         entity.managedObjectClassName = entityName
 
         // Create the attributes
+        var properties: [NSPropertyDescription] = []
+
         let timestampAttribute = NSAttributeDescription()
         timestampAttribute.name = "timestamp"
         timestampAttribute.attributeType = .dateAttributeType
         timestampAttribute.isOptional = false
+        properties.append(timestampAttribute)
+
+        let idAttribute = NSAttributeDescription()
+        idAttribute.name = "id"
+        idAttribute.attributeType = .UUIDAttributeType
+        idAttribute.isOptional = false
+        properties.append(idAttribute)
 
         // Add properties to entity
-        entity.properties = [
-            timestampAttribute
-        ]
+        entity.properties = properties
 
         return entity
     }()
